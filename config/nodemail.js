35494959dd2b85b2e87becd1a6cc6cae4b67df26
Samarth Kashyap <93036927+samarthkashyap-1@users.noreply.github.com>
2024-09-30 12:49:03 +0530

    Update nodemail.js

--- config/nodemail.js
+++ config/nodemail.js
@@ -3,10 +3,15 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
   },
+  logger: true,
 });
 
 module.exports = transporter
